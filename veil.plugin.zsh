89120bd63e14c6c6dd23da30d837262847eb0a5e
Egor Lem <torridtime@gmail.com>
2025-12-24 20:21:55 +0300

    Fix shellcheck issues

--- veil.plugin.zsh
+++ veil.plugin.zsh
@@ -6,12 +6,14 @@
 # ------------------------------------------------------------------------------
 
 # Set load mode to plugin
+# shellcheck disable=SC2034
 typeset -g VEIL_MODE="plugin"
 
 # Path to the Veil core
 typeset -g VEIL_CORE_FILE="${0:A:h}/veil.zsh"
 
 veilCoreInit() {
+  # shellcheck disable=SC1090
   if source "$VEIL_CORE_FILE"; then
     [[ -n "$VEIL_VERBOSE" ]] && echo "veil: core loaded successfully in plugin mode"
     return 0
